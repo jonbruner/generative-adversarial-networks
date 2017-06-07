ec7d59e051ade4f87e62fb62307b128a5da2b609
Jon Bruner <jon.bruner@gmail.com>
2017-06-07 10:53:58 -0700

    Fix variable scope error

--- gan-script.py
+++ gan-script.py
@@ -22,43 +22,44 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/")
 
 # Define the discriminator network
-def discriminator(images, reuse=False):
-    if (reuse):
-        tf.get_variable_scope().reuse_variables()
+def discriminator(images, reuse_variables=None):
+    #if (reuse):
+    #    tf.get_variable_scope().reuse_variables()
 
-    # First convolutional and pool layers
-    # This finds 32 different 5 x 5 pixel features
-    d_w1 = tf.get_variable('d_w1', [5, 5, 1, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    d_b1 = tf.get_variable('d_b1', [32], initializer=tf.constant_initializer(0))
-    d1 = tf.nn.conv2d(input=images, filter=d_w1, strides=[1, 1, 1, 1], padding='SAME')
-    d1 = d1 + d_b1
-    d1 = tf.nn.relu(d1)
-    d1 = tf.nn.avg_pool(d1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    with tf.variable_scope(tf.get_variable_scope(), reuse=reuse_variables) as scope:
+        # First convolutional and pool layers
+        # This finds 32 different 5 x 5 pixel features
+        d_w1 = tf.get_variable('d_w1', [5, 5, 1, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        d_b1 = tf.get_variable('d_b1', [32], initializer=tf.constant_initializer(0))
+        d1 = tf.nn.conv2d(input=images, filter=d_w1, strides=[1, 1, 1, 1], padding='SAME')
+        d1 = d1 + d_b1
+        d1 = tf.nn.relu(d1)
+        d1 = tf.nn.avg_pool(d1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    # Second convolutional and pool layers
-    # This finds 64 different 5 x 5 pixel features
-    d_w2 = tf.get_variable('d_w2', [5, 5, 32, 64], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    d_b2 = tf.get_variable('d_b2', [64], initializer=tf.constant_initializer(0))
-    d2 = tf.nn.conv2d(input=d1, filter=d_w2, strides=[1, 1, 1, 1], padding='SAME')
-    d2 = d2 + d_b2
-    d2 = tf.nn.relu(d2)
-    d2 = tf.nn.avg_pool(d2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        # Second convolutional and pool layers
+        # This finds 64 different 5 x 5 pixel features
+        d_w2 = tf.get_variable('d_w2', [5, 5, 32, 64], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        d_b2 = tf.get_variable('d_b2', [64], initializer=tf.constant_initializer(0))
+        d2 = tf.nn.conv2d(input=d1, filter=d_w2, strides=[1, 1, 1, 1], padding='SAME')
+        d2 = d2 + d_b2
+        d2 = tf.nn.relu(d2)
+        d2 = tf.nn.avg_pool(d2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    # First fully connected layer
-    d_w3 = tf.get_variable('d_w3', [7 * 7 * 64, 1024], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    d_b3 = tf.get_variable('d_b3', [1024], initializer=tf.constant_initializer(0))
-    d3 = tf.reshape(d2, [-1, 7 * 7 * 64])
-    d3 = tf.matmul(d3, d_w3)
-    d3 = d3 + d_b3
-    d3 = tf.nn.relu(d3)
+        # First fully connected layer
+        d_w3 = tf.get_variable('d_w3', [7 * 7 * 64, 1024], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        d_b3 = tf.get_variable('d_b3', [1024], initializer=tf.constant_initializer(0))
+        d3 = tf.reshape(d2, [-1, 7 * 7 * 64])
+        d3 = tf.matmul(d3, d_w3)
+        d3 = d3 + d_b3
+        d3 = tf.nn.relu(d3)
 
-    # Second fully connected layer
-    d_w4 = tf.get_variable('d_w4', [1024, 1], initializer=tf.truncated_normal_initializer(stddev=0.02))
-    d_b4 = tf.get_variable('d_b4', [1], initializer=tf.constant_initializer(0))
-    d4 = tf.matmul(d3, d_w4) + d_b4
+        # Second fully connected layer
+        d_w4 = tf.get_variable('d_w4', [1024, 1], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        d_b4 = tf.get_variable('d_b4', [1], initializer=tf.constant_initializer(0))
+        d4 = tf.matmul(d3, d_w4) + d_b4
 
-    # d4 contains unscaled values
-    return d4
+        # d4 contains unscaled values
+        return d4
 
 # Define the generator network
 def generator(z, batch_size, z_dim):
@@ -97,6 +98,8 @@ def generator(z, batch_size, z_dim):
     # Dimensions of g4: batch_size x 28 x 28 x 1
     return g4
 
+print(tf.get_variable_scope().reuse)
+
 z_dimensions = 100
 batch_size = 50
 z_placeholder = tf.placeholder(tf.float32, [None, z_dimensions], name='z_placeholder')
@@ -112,7 +115,7 @@ Dx = discriminator(x_placeholder)
 # Dx will hold discriminator prediction probabilities
 # for the real MNIST images
 
-Dg = discriminator(Gz, reuse=True)
+Dg = discriminator(Gz, reuse_variables=True)
 # Dg will hold discriminator prediction probabilities for generated images
 
 # Define losses
@@ -126,13 +129,17 @@ d_vars = [var for var in tvars if 'd_' in var.name]
 g_vars = [var for var in tvars if 'g_' in var.name]
 
 # Define the optimizers
-with tf.variable_scope(tf.get_variable_scope(), reuse=False) as scope:
+with tf.variable_scope("optimizers") as scope:
+    print(scope)
+    print(scope.reuse)
     # Train the discriminator
     d_trainer_fake = tf.train.AdamOptimizer(0.0003).minimize(d_loss_fake, var_list=d_vars)
     d_trainer_real = tf.train.AdamOptimizer(0.0003).minimize(d_loss_real, var_list=d_vars)
 
     # Train the generator
     g_trainer = tf.train.AdamOptimizer(0.0001).minimize(g_loss, var_list=g_vars)
+
+tf.get_variable_scope().reuse_variables()
 
 sess = tf.Session()
 
