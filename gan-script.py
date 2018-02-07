6f19660a6232005a400a597991e7a382770b7191
Jon Bruner <jon.bruner@gmail.com>
2018-02-07 11:15:44 -0800

    Correct scope parameter in generator batch_norm layers

--- gan-script.py
+++ gan-script.py
@@ -64,7 +64,7 @@ def generator(z, batch_size, z_dim):
     g_b1 = tf.get_variable('g_b1', [3136], initializer=tf.truncated_normal_initializer(stddev=0.02))
     g1 = tf.matmul(z, g_w1) + g_b1
     g1 = tf.reshape(g1, [-1, 56, 56, 1])
-    g1 = tf.contrib.layers.batch_norm(g1, epsilon=1e-5, scope='bn1')
+    g1 = tf.contrib.layers.batch_norm(g1, epsilon=1e-5, scope='g_b1')
     g1 = tf.nn.relu(g1)
 
     # Generate 50 features
@@ -72,7 +72,7 @@ def generator(z, batch_size, z_dim):
     g_b2 = tf.get_variable('g_b2', [z_dim/2], initializer=tf.truncated_normal_initializer(stddev=0.02))
     g2 = tf.nn.conv2d(g1, g_w2, strides=[1, 2, 2, 1], padding='SAME')
     g2 = g2 + g_b2
-    g2 = tf.contrib.layers.batch_norm(g2, epsilon=1e-5, scope='bn2')
+    g2 = tf.contrib.layers.batch_norm(g2, epsilon=1e-5, scope='g_b2')
     g2 = tf.nn.relu(g2)
     g2 = tf.image.resize_images(g2, [56, 56])
 
@@ -81,7 +81,7 @@ def generator(z, batch_size, z_dim):
     g_b3 = tf.get_variable('g_b3', [z_dim/4], initializer=tf.truncated_normal_initializer(stddev=0.02))
     g3 = tf.nn.conv2d(g2, g_w3, strides=[1, 2, 2, 1], padding='SAME')
     g3 = g3 + g_b3
-    g3 = tf.contrib.layers.batch_norm(g3, epsilon=1e-5, scope='bn3')
+    g3 = tf.contrib.layers.batch_norm(g3, epsilon=1e-5, scope='g_b3')
     g3 = tf.nn.relu(g3)
     g3 = tf.image.resize_images(g3, [56, 56])
 
